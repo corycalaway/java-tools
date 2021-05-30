c77011383df43a4120988fd2a345f5c8c53ee4d2
Cory Calaway <cory.c.calaway@gmail.com>
2021-05-30 12:30:58 -0600

    added notes to java basics

--- src/com/company/Java_basics.java
+++ src/com/company/Java_basics.java
@@ -2,9 +2,9 @@ package com.company;
 
 public class Java_basics {
 
+
     //method
     public static void myJavaNotes() {
-
 
         System.out.println("Hello World");
 
@@ -21,6 +21,31 @@ public class Java_basics {
         System.out.println(firstInitial);
         System.out.println(name);
         System.out.println(isHome);
+
+    }
+
+    public static void myJavaTryCatch() {
+
+
+        String face = "Perfect";
+        // trys this code
+        try {
+            // prints face variable
+            System.out.println(face);
+
+            // creates an array
+            int[] myNumbers = {1, 2, 3};
+            // fails due to myNumbers[10] not existing
+            System.out.println(myNumbers[10]);
+
+            // if fails catches
+        } catch(Exception e) {
+            System.out.println("Fail");
+
+            // executes after trycatch is complete
+        } finally {
+            System.out.println("Done");
+        }
 
     }
 
