71454e622e19123acceea7ef42abfbce540a1bb1
Cory Calaway <cory.c.calaway@gmail.com>
2021-05-30 11:57:37 -0600

    first commit java tools

---
+++ src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Java_basics JavaNotes = new Java_basics();
+        String notes = JavaNotes.myJavaNotes();
+        System.out.println(notes);
+//
+//        System.out.println("Hello World");
+//
+//    // Variables
+//    int num = 5;
+//    double doubley = 4.6;
+//    char firstInitial = 'K';
+//    String name = "Cory";
+//    boolean isHome = true;
+//
+//    // Prints num variable
+//        System.out.println(num);
+//        System.out.println(doubley);
+//        System.out.println(firstInitial);
+//        System.out.println(name);
+//        System.out.println(isHome);
+    }
+}
